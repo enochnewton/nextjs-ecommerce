42223cb8e65d679904782aba950e3dffb8f365bb
enochnewton <enochnewton2@gmail.com>
2023-02-08 10:29:21 +0300

    '7th commit'

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,16 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang='en'>
+      <Head>
+        <link rel='manifest' href='/manifest.json' />
+        <link rel='theme-color' href='#fff' />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
