6911884423e017da21111b3100e4957ba4fed4fb
enochnewton <enochnewton2@gmail.com>
2023-02-08 10:13:41 +0300

    '7th commit'

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-};
+const runtimeCaching = require("next-pwa/cache");
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  runtimeCaching,
+  buildExcludes: [/middleware-manifest.json$/],
+  disable: process.env.NODE_ENV === "development",
+});
 
+const nextConfig = withPWA({
+  reactStrictMode: true,
+  swcMinify: true,
+});
 module.exports = nextConfig;
